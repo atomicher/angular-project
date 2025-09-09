bdad81351786f6f11dd0aa3228fdbe19dde0cfd0
Анна Чернявська <146863432+atomicher@users.noreply.github.com>
2025-09-09 22:25:17 +0200

    Update layout.ts

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  templateUrl: './layout.html',
+  styleUrls: ['./layout.css'],
+  imports: [RouterModule]
+
 })
 export class LayoutComponent {
   appTitle = 'Подорожі';
